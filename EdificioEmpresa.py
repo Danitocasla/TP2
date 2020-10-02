db379eb4f80f7c31a7d3c5a3e060e03e4031ad2e
Daniel Mendez <rdmmendez@hotmail.com>
2020-10-02 19:01:20 -0300

    Agregando archivo EsdificioEmpresa

---
+++ EdificioEmpresa.py
@@ -0,0 +1,26 @@
+
+
+
+class EdificioEmpresa():
+    def __init__(self):
+        pass
+    
+    def establecerOficina(self, numeroPiso, numeroHabitaculo, oficinaAtencion):
+        pass
+    
+    def cantidadDeOficinasCriticas(self, piso):
+        pass
+    
+    def oficinaMenosRecargada(self):
+        pass
+    
+    def buscaOficina(self, nroInterno):
+        pass
+    
+    def centralTelefonica(self, pilaDeAuxilios):
+        pass
+    
+    def moverAuxilio(self, nroPatente, internoOficinaOrigen, internoOficinaDestino):
+        pass
+    
+    
