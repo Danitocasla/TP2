b0817ada9fb57786281db5e1af150bc5a0de99d3
Daniel Mendez <rdmmendez@hotmail.com>
2020-10-05 22:27:50 -0300

    Redefinicion de Código

--- Auxilios.py
+++ Auxilios.py
@@ -1,24 +1,10 @@
-from enum import Enum
-import Zonas
-
-# USO DE ENUM
-class TipoAuxilio(int,Enum):
-    Reparacion = 0
-    Remolque = 1
-
-class Estado(int,Enum):
-    espera = 0
-    aprobado = 1
-
-class Estado(int,Enum):
-    espera = 0
-    aprobado = 1
-
 # DEFINICION DE AUXILIOS
 class Auxilio():
-    def __init__(self, patente, zonaPartida, estado):
+    def __init__(self, patente, zonaPartida, zonaDestino, tipo, estado):
         self.patente = patente
         self.zonaPartida = zonaPartida
+        self.zonaDestino = zonaDestino
+        self.tipo = tipo
         self.estado = estado
     
     def enEspera(self):
@@ -27,25 +13,12 @@ class Auxilio():
     def zonaPartida(self):
         return self.zonaPartida
 
-class Remolque(Auxilio):
-    def __init__(self, patente, zonaPartida, zonaLlegada, estado):
-        super().__init__(patente, zonaPartida, estado)
-        self.zonaLlegada = zonaLlegada
-        self.tipoAuxilio = TipoAuxilio(1)
-    
-
-class Reparacion(Auxilio):
-    def __init__(self, patente, zonaPartida, estado):
-        super().__init__(patente, zonaPartida, estado)
-        self.zonaLlegada = zonaPartida
-        self.tipoAuxilio = TipoAuxilio(0)
-
-# ACCESORIOS A AUXILIOS
-class Patente():
-    def __init__(self, letras, numero):
-        self.letras = letras
-        self.numero = numero
-    
-    def getNumero(self):
-        return self.numero
+# VER SI ESTO SIRVE
+#class Patente():
+#    def __init__(self, letras, numero):
+#        self.letras = letras
+#        self.numero = numero
+#    
+#    def getNumero(self):
+#        return self.numero
         
