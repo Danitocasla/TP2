6fa18b231b79855592ee760cb4e99f709ad28088
Daniel Mendez <rdmmendez@hotmail.com>
2020-10-07 16:50:16 -0300

    Cambio para que funcione

--- OficinaAtencion.py
+++ OficinaAtencion.py
@@ -22,14 +22,9 @@ class OficinaAtencion():
     ###################################################################
 
     def recibirAuxilio(self, auxilio):
-<<<<<<< HEAD
-        if type(auxilio.unTipo)==type(TipoAuxilio.Reparacion):
-            self.situacionCritica()
-=======
 
-        if auxilio.tipo() == TipoAuxilio.Remolque:
-            situacionCritica()
->>>>>>> c2673e965638d96aa25f02016519eacbf6375bab
+        if auxilio.unTipo == TipoAuxilio.Remolque:
+            self.situacionCritica()
             self.colaRemolque.enqueue(auxilio)
         else:
             self.situacionCritica()
