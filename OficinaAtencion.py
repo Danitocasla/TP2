c805018ebbe846023acd6b468654885d90334de5
Daniel Mendez <rdmmendez@hotmail.com>
2020-10-07 01:21:17 -0300

    AF agregando funcionalidad y corrigiendo algún error

--- OficinaAtencion.py
+++ OficinaAtencion.py
@@ -1,5 +1,6 @@
 from Queue import *
 from Auxilios import *
+from Tipos import *
 
 # 2 colas: colaRemolque y colaReparacion
 # interno de la central: 1 a 999
@@ -26,7 +27,7 @@ class OficinaAtencion():
     ###################################################################
 
     def recibirAuxilio(self, auxilio):
-        if auxilio.tipo == 0:
+        if auxilio.tipo() == TipoAuxilio.Remolque:
             situacionCritica()
             self.colaRemolque.enqueue(auxilio)
         else:
@@ -41,22 +42,22 @@ class OficinaAtencion():
             salida = self.colaReparacion.top()
         return salida
 
-# recibe la zona donde se encuentra una grua
-# devuelve y desencola el primer auxilio que se le puede enviar,
-# la zonaDeGrua debe ser la zona De Partida del auxilio. remolques tienen prioridad
+    # recibe la zona donde se encuentra una grua
+    # devuelve y desencola el primer auxilio que se le puede enviar,
+    # la zonaDeGrua debe ser la zona De Partida del auxilio. remolques tienen prioridad
     def enviarAuxilio(self, zonaDeGrua):
         pass
 
-# retorna la cantidad de auxilios de cada tipo:, ej: Remolque: n ; Reparacion: n
+    # retorna la cantidad de auxilios de cada tipo:, ej: Remolque: n ; Reparacion: n
     def auxiliosPorTipo(self):
         # Ver el orden que se espera y si es lo esperado
         return self.colaRemolque.size(), self.colaReparacion.size()
 
-# retorna la cantidad sumando las dos colas
+    # retorna la cantidad sumando las dos colas
     def cantidadTotalAuxilios(self):
         return self.colaRemolque.size() + self.colaReparacion.size()
 
-# retorna true si alguna de las dos colas supera la cant critica
+    # retorna true si alguna de las dos colas supera la cant critica
     def esCritica(self):
         return self.esCriticaRemolque() or self.esCriticaReparacion()
     # funciones auxiliares
@@ -67,31 +68,47 @@ class OficinaAtencion():
     def esCriticaReparacion(self):
         return self.colaReparacion.size() >= self.cantCritica
 
-# retorna el total de auxilios con estado: espera. sumando las dos colas
+    # retorna el total de auxilios con estado: espera. sumando las dos colas
     def auxiliosEnEspera(self):
         return self.contarEnEspera(self.colaRemolque) + self.contarEnEspera(self.colaReparacion)
 
-# recibe una patente
-# retorna(sin eliminarlo) el auxilio pedido por esa patente si hay alguno en las colas
+    # recibe una patente
+    # retorna(sin eliminarlo) el auxilio pedido por esa patente si hay alguno en las colas
     def buscarAuxilio(self, nroPatente):
-        pass
+        salida = None
+        for i in range(self.colaRemolque.size):
+            if self.colaRemolque[i].patente() == nroPatente:
+                salida = self.colaRemolque[i]
+        if salida == None:
+            for i in range(self.colaReparacion.size):
+                if self.colaReparacion[i].patente() == nroPatente:
+                    salida = self.colaReparacion[i]
+        return salida
 
-# recibe una patente
-# elimina el auxilio pedido por esa patente  si hay.
+    # recibe una patente
+    # elimina el auxilio pedido por esa patente  si hay.
     def eliminarAuxilio(self, nroPatente):
-        pass
+        auxilio = self.buscarAuxilio(nroPatente)
+        if auxilio != None:
+            if self.colaRemolque.index(auxilio) != None:
+                self.colaRemolque.eliminar(self.colaRemolque.index(auxilio))
+            else:
+                self.colaReparacion.eliminar(
+                    self.colaReparacion.index(auxilio))
 
-# recibe una patente
-# verifica que exista un pedido de esa patente
-# lo cambia de cola(reparacion - remolque)
+    # recibe una patente
+    # verifica que exista un pedido de esa patente
+    # lo cambia de cola(reparacion - remolque)
     def cambiaDeTipo(self, nroPatente):
-        pass
+        #auxilio = self.buscarAuxilio(nroPatente)
+        #if auxilio != None:
+        #    clon = self.c
+        pass    
+        
 
     def situacionCritica(self):
         if self.esCritica():
             print("Situación Crírtica")
-        else:
-            pass
 
     def contarEnEspera(self, cola):
         clon = cola.clone()
