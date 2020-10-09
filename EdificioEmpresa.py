6a23751a6b6ac8f1ef90b7240b301c85e9f5fd92
Daniel Mendez <rdmmendez@hotmail.com>
2020-10-09 08:31:09 -0300

    Actualizacion

--- EdificioEmpresa.py
+++ EdificioEmpresa.py
@@ -25,28 +25,43 @@ class EdificioEmpresa():
     def establecerOficina(self, numeroPiso, numeroHabitaculo, oficinaAtencion):
         self.edificioEmpresa[numeroPiso][numeroHabitaculo] = oficinaAtencion
 
-    # RECURSIVA
+    
     # retorna la cantidad de oficinas en situacion critica en el piso recibido
     def cantidadDeOficinasCriticas(self, piso):
-        pass
+        count = 0
+        for i in range(len(self.edificioEmpresa[piso])):
+            if self.edificioEmpresa[piso][i]!= None:
+                if self.edificioEmpresa[piso][i].esCritica():
+                    count+=1
+        return count
 
     # retorna piso y habitaculo de la oficina con menor cant de auxilios tipo Remolque pendientes
     # hay que recorrer la matriz(self.edificioEmpresa) y devolver la
     # oficina.cantidadTotalAuxilios()
     def oficinaMenosRecargada(self):
-        pass
+        salida = None
+        oficina = None
+        cantTotAux = 0
+        for nroPiso in range(len(self.edificioEmpresa)):
+            for habitaculo in range(len(self.edificioEmpresa[nroPiso])):
+                pass
+        
 
     # recibe en un nro de interno
     # retorna el piso y habitaculo donde se encuentra
+
+    def hayOficinaEn(self, nroPiso, nroHabitaculo):
+        return self.edificioEmpresa[nroPiso][nroHabitaculo] != None
+
+
     def buscaOficina(self, nroInterno):
-        """for i in range(self.cantPisos):
+        for i in range(self.cantPisos):
             for j in range(self.cantHabitaculos):
                 if self.hayOficinaEn(i, j):
 
                     if (self.edificioEmpresa[i][j]).interno == nroInterno:
-                        return self.edificioEmpresa[i][j]
-                    """
-        pass
+                        salida = i , j
+        return salida
 
     # recibe una PILA de auxilios y los reparte de a uno a la oficina menos recargada
     # (verificar entre cada entrega cual es la menos recargada)
@@ -57,10 +72,9 @@ class EdificioEmpresa():
     # recibe un nro de patente, un nro interno de origen y otro nro de interno de destino
     # mueve el auxilio de esa patente desde el interno de origen al interno de destino
     def moverAuxilio(self, nroPatente, internoOficinaOrigen, internoOficinaDestino):
-        oficinaOrigen = self.buscaOficina(internoOficinaOrigen)
-        oficinaDestino = self.buscaOficina(internoOficinaDestino)
+        pisoOrigen, habOrigen = self.buscaOficina(internoOficinaOrigen)
+        pisoDestino, habDestino = self.buscaOficina(internoOficinaDestino)
+        oficinaOrigen = self.edificioEmpresa[pisoOrigen][habOrigen]
+        oficinaDestino = self.edificioEmpresa[pisoDestino][habDestino]
         oficinaDestino.recibirAuxilio(oficinaOrigen.buscarAuxilio(nroPatente))
         oficinaOrigen.eliminarAuxilio(nroPatente)
-
-    def hayOficinaEn(self, nroPiso, nroHabitaculo):
-        return self.edificioEmpresa[nroPiso][nroHabitaculo] != None
